8828bfb50edcc23be7907da63622aac629ef3378
Frederico Martini <fredmalmeida@gmail.com>
2019-09-01 18:40:57 -0300

    andamento

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const EditExpense = () => <div>This is the edit expense</div>;
+const EditExpense = (props) => {
+  const {
+    match: {
+      params: { id },
+    },
+  } = props;
+  console.log(props);
+  return (
+    <div>
+      This is the edit expense for the item:
+      {id}
+    </div>
+  );
+};
 
 export default EditExpense;
